e7d53fa0a1c3585dde2a70bee5031629603d885e
hannahm-04 <hannahanifam@gmail.com>
2026-02-11 08:21:46 +0700

    fix: update validator logic

--- src/logic/validator.js
+++ src/logic/validator.js
@@ -1,17 +1,16 @@
-git add .
-git commit -m "fix: prevent freeze on solve cell and check by validating constraints first"
-git push origin mainexport const isValid = (board, row, col, num) => {
-  for (let i = 0; i < 9; i++) {
-    // Cek Baris
-    if (board[row][i] === num) return false
+export const isValid = (board, row, col, num) => {
+    for (let i = 0; i < 9; i++) {
+        // Cek Baris
+        if (board[row][i] === num) return false;
 
-    // Cek Kolom
-    if (board[i][col] === num) return false
+        // Cek Kolom
+        if (board[i][col] === num) return false;
 
-    // Cek Sub-grid 3x3
-    const boxRow = Math.floor(row / 3) * 3
-    const boxCol = Math.floor(col / 3) * 3
-    if (board[boxRow + Math.floor(i / 3)][boxCol + (i % 3)] === num) return false
-  }
-  return true
-}
+        // Cek Sub-grid 3x3
+        const boxRow = Math.floor(row / 3) * 3;
+        const boxCol = Math.floor(col / 3) * 3;
+        if (board[boxRow + Math.floor(i / 3)][boxCol + (i % 3)] === num)
+            return false;
+    }
+    return true;
+};
